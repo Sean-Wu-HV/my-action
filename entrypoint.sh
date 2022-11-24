214ce8332852f460dbd754fd771405cb7bbf6491
Sean Wu <sean.wu@hitachivantara.com>
2022-11-23 16:09:22 -0800

    cc

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -l
 
+echo "parsing inputs"
+
+
 echo "Hello $1"
+echo "your classroom is $2"
+echo "gradle version: $(gradle -version)" 
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
