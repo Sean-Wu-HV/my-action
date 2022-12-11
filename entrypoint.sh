e1be459cfc148069805eb3d535ece291b8387f3a
Sean-Wu-HV <106768445+Sean-Wu-HV@users.noreply.github.com>
2022-12-11 10:37:08 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,10 @@ echo "your classroom is $2"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 
-echo "$(ls -la)"
-echo "$(ls -la /)"
-echo "$(pwd)"
-echo "$(whoami)"
+echo "ls -la $(ls -la)"
+echo "ls -la / $(ls -la /)"
+echo "pwd $(pwd)"
+echo "whoami $(whoami)"
 
 echo "$(java --version)"
 echo "$(docker ps)"
