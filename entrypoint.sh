f214243cf8adbb96e9f8d480a7f4fba0fcef03a3
Sean-Wu-HV <106768445+Sean-Wu-HV@users.noreply.github.com>
2022-12-10 21:56:52 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,7 @@ echo "your classroom is $2"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 
-curl -O https://detect.synopsys.com/detect8.sh
-
-chmod u+x detect8.sh
-./detect8.sh -h
-
 echo "$(ls -la)"
+
+echo "(java --version)"
+echo "docker ps"
