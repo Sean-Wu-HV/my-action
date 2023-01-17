3c9f1576004b3ce87fbb6aade2fbac363426ea62
Sean-Wu-HV <106768445+Sean-Wu-HV@users.noreply.github.com>
2023-01-17 23:02:37 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,3 @@
 #!/bin/sh -l
 
-# fail script if error
-set -eo pipefail
-
-echo "parsing inputs"
-
-
-echo "Hello $1"
-echo "your classroom is $2"
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
-
-echo "ls -la $(ls -la)"
-echo "ls -la / $(ls -la /)"
-echo "pwd $(pwd)"
-echo "whoami $(whoami)"
-
-echo "$(java --version)"
-echo "$(docker ps)"
-echo "$(docker run hello-world)"
-
-echo $@
-/./testing.sh $@
-# /./detect.sh $@
+poetry config --list
